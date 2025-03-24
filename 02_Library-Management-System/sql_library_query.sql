2ba0d0e75a44dbbeb480accf321e2b5250807ac8
subodhghonge <subodhghonge12@gmail.com>
2025-03-24 21:14:21 +0530

    Queries

--- 02_Library-Management-System/sql_library_query.sql
+++ 02_Library-Management-System/sql_library_query.sql
@@ -26,7 +26,34 @@ SELECT * FROM issued_status
 WHERE issued_emp_id = 'E101';
 
 -- Q5. List Members Who Have Issued More Than One Book -- Objective: Use GROUP BY to find members who have issued more than one book.
-SELECT issued_emp_id -- COUNT(issued_id) as issued_count
+SELECT issued_emp_id --  COUNT(issued_id) as issued_count
 FROM issued_status
 GROUP BY issued_emp_id
 HAVING COUNT(issued_id) > 1;
+
+-- Q6. Create Summary Tables: Used CTAS to generate new tables based on query results - each book and total book_issued_cnt.
+CREATE TABLE book_counts AS
+SELECT b.isbn, b.book_title, COUNT(ist.issued_book_isbn) as no_issued
+FROM books AS b
+JOIN
+issued_status AS ist
+ON ist.issued_book_isbn = b.isbn
+GROUP BY 1,2;
+
+-- Q7. Retrieve All Books in a Specific Category. 
+SELECT * FROM books
+Where category = "History";
+
+-- Q8. Find Total Rental Income by Category
+SELECT b.category, SUM(b.rental_price) AS rental_income
+FROM books as b
+JOIN
+issued_status as ist
+ON ist.issued_book_isbn = b.isbn
+GROUP BY 1;
+
+-- Q9. List Members Who Registered in the Last 180 Days:
+SELECT * FROM members
+WHERE reg_date >= CURRENT_DATE - INTERVAL 180 DAY;
+
+-- Q10. List Employees with Their Branch Manager's Name and their branch details.
